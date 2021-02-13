26cdcdfe23c178366b73d7468ec079f1cacf5fec
aasuz <aasuz@aasuz-PC>
2021-02-13 18:03:39 +0530

    first commit...

---
+++ src/com/gitprojectjava/Test.java
@@ -0,0 +1,15 @@
+package com.gitprojectjava;
+
+public class Test {
+
+	public static void main(String[] args) {
+
+		for (int i = 0; i < args.length; i++) {
+
+			
+			System.out.println("this is my code please update it soon");
+		}
+
+	}
+
+}
